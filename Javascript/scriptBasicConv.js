be331a8572d7161f1c96be1e442c685e9fa6bb77
Lohith N <lohith.n@fractal.ai>
2023-09-28 14:43:06 +0530

    Javascript files

---
+++ Javascript/scriptBasicConv.js
@@ -0,0 +1,51 @@
+(function(){
+
+    "use strict";
+
+    var form = document.getElementById('convert')
+    var convertType = 'kilometers';
+    var heading = document.querySelector('h1');
+    var intro = document.querySelector('p');
+    var answerDiv = document.querySelector('#answer');
+    var direction = document.getElementById('direction');
+
+
+    document.addEventListener('keydown',function(event){
+
+        let key = event.code;
+        // alert(key)
+       if (key === 'KeyM'){
+        convertType = 'miles';
+        heading.innerHTML=`Kilometers to miles converter`;
+        intro.innerHTML='Type in the number of miles and click the button to convert the distance to kilometers'
+        direction.innerHTML=`<h3>Press the "K" key to switch to miles to kilometers convertion</h3>`
+       }
+       else if (key === 'KeyK' )
+       {
+        convertType='kilometers';
+        heading.innerHTML=`Miles to kilomters converter`;
+        intro.innerHTML='Type in the number of kilometers and click the button to convert the distance to miles'
+        direction.innerHTML=`<h3>Press the "M" key to switch to kilometers to miles convertion</h3>`
+
+       }
+    })
+
+    form.addEventListener('submit', function (event) {
+    event.preventDefault();    
+    
+    let distance = parseFloat(document.getElementById('distance').value);
+
+    if (distance){
+        if (convertType==='kilometers'){
+            answerDiv.innerHTML=`<h2>${distance} miles converts to ${(1.60934*distance).toFixed(2)} kilometers</h2>`;
+        }
+        
+        if(convertType==='miles'){
+            answerDiv.innerHTML=`<h2>${distance} kilometer converts to ${(0.621371*distance).toFixed(2)} miles</h2>`;
+        }
+    }
+    else{
+            answerDiv.innerHTML=`<h2>Please provide a number!</h2>`;
+    }
+})
+})();
